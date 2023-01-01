ecc9bf07a0e3513862dc26a7d5c28cd89836eb4d
Dan <daniiar.mamaev@gmail.com>
2023-01-01 15:34:00 +0600

    added signin function

--- e_store/main/models.py
+++ e_store/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Goods(models.Model):
+    goods_choices = [
+        ('Phones', 'Phones'),
+        ('Laptops', 'Laptops'),
+        ('PC', 'PC')
+    ]
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    category = models.CharField(max_length=30, choices=goods_choices)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
--- e_store/main/views.py
+++ e_store/main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from .models import Goods
 
 
 def main_page(request):
-    return render(request, "main/index.html")
+    goods = Goods.objects.all()
+    context = {'goods': goods}
+    return render(request, "main/index.html", context)
 
 
 def login_page(request):
@@ -15,5 +19,16 @@ def login_page(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('main')
 
     return render(request, 'main/login.html', context)
+
+
+def sign_in(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        login(request, user)
+        return redirect('main')
+    return render(request, 'main/sign_in.html')
